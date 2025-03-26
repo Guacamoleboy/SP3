cb3cc603e5919bbfbde60da54044265329b4053e
Guacamoleboy <twitch@galleripermoller.dk>
2025-03-26 17:28:54 +0100

    comments

--- Project/src/Program.java
+++ Project/src/Program.java
@@ -52,9 +52,9 @@ public class Program {
                     maxID = userID;
                 }
 
-            }
+            } // For-loop
 
-        }
+        } // If-statement
 
         userCount = user.size();
         ID = maxID + 1;

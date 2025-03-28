eeb437a031f0179047251a614c9d6663f7938684
Guacamoleboy <twitch@galleripermoller.dk>
2025-03-28 23:06:35 +0100

    FileIO (fixCSV)

--- Project/src/FileIO.java
+++ Project/src/FileIO.java
@@ -6,6 +6,7 @@
     saveData
     readData (ArrayList)
     readData (Array)
+    CSVFixer
 
     Last updated: 28-03-2025
 
@@ -70,6 +71,8 @@ public class FileIO { // Custom generic FileIO
 
     }
 
+    // ________________________________________________________
+
     public String[] readData(String path, int length) {
 
         String[] data = new String[length];
@@ -96,4 +99,97 @@ public class FileIO { // Custom generic FileIO
 
     }
 
-}
+    // ________________________________________________________
+
+    /*
+
+    How to use:
+    ___________
+
+    Overwrite existing file:
+    fixCSV("data/seriesData.csv", "data/seriesData.csv");
+
+    Read BUT output as new file:
+    fixCSV("data/seriesData.csv", "data/seriesData_fixCSV.csv");
+
+    What it does:
+    _____________
+
+    Reads the header LENGTH and checks if each line has the same amount. If the amount is UNDER then
+    it will add a ",0" so each line has the exact same length as the header.
+    This will allow us to have a pretty setup on Github.
+
+    */
+
+    public void fixCSV(String inputFilePath, String outputFilePath) {
+
+        ArrayList<String> fixedLines = new ArrayList<>();
+
+        try {
+
+            BufferedReader br = new BufferedReader(new FileReader(inputFilePath));
+            String header = br.readLine();
+
+            // Checks .csv file for a header
+            if (header == null) {
+
+                System.out.println("Empty CSV file.");
+                return;
+
+            }
+
+            fixedLines.add(header);
+
+            int headerCount = header.split(",", -1).length;
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",", -1);
+                int diff = headerCount - values.length;
+
+                if (diff > 0) {
+
+                    // Add "0" to the end of the array
+                    StringBuilder fixedLine = new StringBuilder(line);
+
+                    for (int i = 0; i < diff; i++) {
+                        fixedLine.append(",0");
+                    }
+
+                    fixedLines.add(fixedLine.toString());
+
+                } else {
+
+                    fixedLines.add(line);
+
+                } // If-else end
+
+            } // While-end
+
+        } catch (IOException e) {
+
+            System.out.println("Error. Please fix and recompile.");
+
+        }
+
+        // Write the fixed lines to outputFilePath
+        try {
+
+            BufferedWriter bw = new BufferedWriter(new FileWriter(outputFilePath));
+
+            for (String fixedLine : fixedLines) {
+                bw.write(fixedLine);
+                bw.newLine();
+            }
+
+            System.out.println("\nSuccess..\nFixed CSV written to: " + outputFilePath);
+
+        } catch (IOException e) {
+
+            System.out.println("Error. Please fix and recompile.");
+
+        }
+
+    }
+
+} // FileIO end

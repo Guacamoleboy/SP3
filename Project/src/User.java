e7a9733ac2af3973339e2055bc4d5636849bd0fb
daud0095 <daudibragimov2004@gmail.com>
2025-04-02 23:11:15 +0200

    Shiiii

--- Project/src/User.java
+++ Project/src/User.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class User {
 
     // Attributes
@@ -22,6 +24,54 @@ public class User {
     } // Constructor
 
     // ________________________________________________________
+
+    /**
+     * Laver Metode til at registrer en ny bruger.
+     * Så brugerene bliver bedt om at indtaste et brugerenavn, alder, køn og password.
+     * Og hvis password ikke matcher bliver brugeren bedt om at prøve igen
+     *
+     *
+     * @return En ny User-intand med brugerenn oplysninger.
+     */
+
+    public static User registerAccount() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+
+        System.out.print("Enter age: ");
+        int age = scanner.nextInt();
+        scanner.nextLine(); // Fanger den newline så input ikke hopper over næste scanner.nextLine()
+
+        System.out.print("Enter gender: ");
+        String gender = scanner.nextLine();
+
+        String password;
+        String confirmPassword;
+
+        // Laver loop som sikre at brugeren indtaster det samme password to gange
+        while (true) {
+            System.out.print("Enter password: ");
+            password = scanner.nextLine();
+
+            System.out.print("Confirm password: ");
+            confirmPassword = scanner.nextLine();
+
+            if (password.equals(confirmPassword)) { // Såh hvis passwords matcher bliver loopet afsluttet
+                System.out.println("Account successfully created!");
+                return new User(username, generateID(), age, gender, "No", password);
+            } else {
+                System.out.println("Passwords do not match. Please try again.");
+            }
+        }
+    }
+     // ______________________________________________________________
+
+    /**
+     * Konverterer brugerens oplysninger til en CSV format stren, som fc kan bruges til filgaming.
+     * @return
+     */
 
     public String toCSV(){
 
@@ -67,4 +117,14 @@ public class User {
         return this.banned;
     }
 
+    /**
+     * Genererer et tilfældigt ID til brugeren
+     * @return Et unikt ID
+     */
+    private static int generateID() {
+        return (int) (Math.random() * 10000); // Genererer en tilfældige ID mellem 0 og 9999
+    }
+
 }
+
+

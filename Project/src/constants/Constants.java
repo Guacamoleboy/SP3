3e853373ba4c0885067e4831cccf26dc3ad1a7f1
Guacamoleboy <twitch@galleripermoller.dk>
2025-04-03 11:39:17 +0200

    Ged
    Ged

--- Project/src/constants/Constants.java
+++ Project/src/constants/Constants.java
@@ -3,6 +3,6 @@ package constants;
 public class Constants {
 
     // Attributes
-    public static final String versionControl = "\nVersion 0.2.0\n_____________";
+    public static final String versionControl = "\nVersion 0.3.0\n_____________";
 
 }

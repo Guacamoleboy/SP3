fd4888e40f134794e45647caf3ec00583484133f
daud0095 <daudibragimov2004@gmail.com>
2025-04-04 12:23:09 +0200

    Hello

--- Project/src/User.java
+++ Project/src/User.java
@@ -12,15 +12,13 @@ public class User {
 
     // ________________________________________________________
 
-    public User(String username, int ID, int age, String gender, String banned, String password){
-
+    public User(String username, int ID, int age, String gender, String banned, String password) {
         this.username = username;
         this.ID = ID;
         this.age = age;
         this.gender = gender;
         this.banned = banned;
         this.password = password;
-
     } // Constructor
 
     // ________________________________________________________
@@ -30,10 +28,8 @@ public class User {
      * Så brugerene bliver bedt om at indtaste et brugerenavn, alder, køn og password.
      * Og hvis password ikke matcher bliver brugeren bedt om at prøve igen
      *
-     *
-     * @return En ny User-intand med brugerenn oplysninger.
+     * @return En ny User-intand med brugerens oplysninger.
      */
-
     public static User registerAccount() {
         Scanner scanner = new Scanner(System.in);
 
@@ -41,19 +37,18 @@ public class User {
         String username = scanner.nextLine();
 
         int age;
-        // Laver en løkkke der sikrer at alderen er mellem 12 og 120
+        // Laver en løkke der sikrer at alderen er mellem 12 og 120
         while (true) {
             System.out.print("Enter age: ");
             age = scanner.nextInt();
             scanner.nextLine();
 
             if (age >= 12 && age <= 120) {
-                break; // Stopper loopet her, hvis alderen er gyldige
+                break; // Stopper loopet her, hvis alderen er gyldig
             } else {
                 System.out.println("Invalid age. Age must be between 12 and 120. Please try again.");
             }
         }
-    }
 
         System.out.print("Enter gender: ");
         String gender = scanner.nextLine();
@@ -73,58 +68,52 @@ public class User {
             } else {
                 System.out.println("Passwords do not match. Please try again.");
             }
-          }
-       }
-
+        }
     }
-     // ______________________________________________________________
+
+    // ______________________________________________________________
 
     /**
-     * Konverterer brugerens oplysninger til en CSV format stren, som fc kan bruges til filgaming.
-     * @return
+     * Konverterer brugerens oplysninger til en CSV format streng, som kan bruges til filgaming.
+     * @return CSV streng
      */
-
-    public String toCSV(){
-
+    public String toCSV() {
         return this.username + ", " + this.ID + ", " + this.age + ", " + this.gender + ", " + this.password + ", " + this.banned;
-
     }
 
     // ________________________________________________________
 
-    public String toCSVSuggest(String value, int ID, String added){
-
+    public String toCSVSuggest(String value, int ID, String added) {
         return value + ", " + ID + ", " + added;
-
     }
 
     // ________________________________________________________
 
-    public String getName(){
+    public String getName() {
         return this.username;
     }
 
     // ________________________________________________________
 
-    public int getID(){
+    public int getID() {
         return this.ID;
     }
 
     // ________________________________________________________
 
-    public int getAge(){
+    public int getAge() {
         return this.age;
     }
 
     // ________________________________________________________
 
-    public String getGender(){
+    public String getGender() {
         return this.gender;
     }
 
     // ________________________________________________________
 
-    public String getBanned(){
+    public String getBanned() {
         return this.banned;
     }
 
@@ -133,9 +122,6 @@ public class User {
      * @return Et unikt ID
      */
     private static int generateID() {
-        return (int) (Math.random() * 10000); // Genererer en tilfældige ID mellem 0 og 9999
+        return (int) (Math.random() * 10000); // Genererer en tilfældig ID mellem 0 og 9999
     }
-
 }
-
-

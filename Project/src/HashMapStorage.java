6a7e44104d821c66e5da3cf46072e30247bad3fd
Guacamoleboy <twitch@galleripermoller.dk>
2025-04-05 14:03:57 +0200

    HashMap to store Confirmation Codes

---
+++ Project/src/HashMapStorage.java
@@ -0,0 +1,47 @@
+/*
+
+    Stores our temp codes in HashMap
+    Limit: 5min
+    Works: As long as app is open
+
+    Made by: Jonas
+    Date: 05-04-2025
+
+*/
+
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+import util.*;
+
+public class HashMapStorage {
+
+    // Attributes
+    private static final TextUI ui = new TextUI();
+    private static final long currentTime = System.currentTimeMillis();
+    private static final HashMap<String, Long> storage = new HashMap<String, Long>();
+    private static final long exp_time = TimeUnit.MINUTES.toMillis(5); // 5min
+
+    // ________________________________________________________
+
+    public static void saveCode(String input){
+
+        storage.put(input, currentTime);
+
+    }
+
+    // ________________________________________________________
+
+    public static boolean validation(String input){
+
+        Long ts = storage.get(input);
+        if (ts == null){
+            ui.displayMsg("Code not found.. Try again.");
+            return false;
+        }
+
+        // Checks if HashMap time vs currentTime is over 5 minutes == expired
+        return (currentTime - ts) <= exp_time;
+
+    }
+
+}

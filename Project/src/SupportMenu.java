b113e91c615d59d32e074078c7195a4a18250a29
Guacamoleboy <twitch@galleripermoller.dk>
2025-04-07 12:08:39 +0200

    Support menu automatic and done
    Let's gooooooooooooooooooo

--- Project/src/SupportMenu.java
+++ Project/src/SupportMenu.java
@@ -16,7 +16,7 @@ public class SupportMenu extends Menu{
         ui.displayMsg("\nWelcome to the Support Menu, " + ui.promptTextColor("red") + username + ui.promptTextColor("reset"));
         ui.displayMsg("_______________________________________\n");
 
-        ui.displayMsg("Open Tickets: 1\nSolved Tickets: 0\n__________________________________\n1) Answer Tickets\n2) Log Out\n");
+        ui.displayMsg("Open Tickets: " + getTicketsByStatus("Open") +  "\nSolved Tickets: " + getTicketsByStatus("Solved") +  "\n__________________________________\n1) Answer Tickets\n2) Log Out\n");
 
         String input = ui.promptTextLine("Input: ");
 
@@ -33,10 +33,17 @@ public class SupportMenu extends Menu{
                     if(!data.isEmpty()){
 
                         String statusColor = " ";
+                        int i = 0;
 
                         if(values[3].equalsIgnoreCase("Open")){
+
                             statusColor = "green";
-                        } else if (values[3].equalsIgnoreCase("Closed")){
+
+                            i++;
+                            ui.displayMsg(i + ") Ticket ID: " + values[0] + " | Message: " + values[2] + "\nStatus: " + ui.promptTextColor(statusColor)
+                            + values[3] + ui.promptTextColor("reset") + "\n");
+
+                        } else if (values[3].equalsIgnoreCase("Solved")){
                             statusColor = "red";
                         } else if (values[3].equalsIgnoreCase("Pending")){
                             statusColor = "yellow";
@@ -44,12 +51,45 @@ public class SupportMenu extends Menu{
                             ui.displayMsg("Something went wrong.. Dev Info: !VALIDCOLOR");
                         }
 
-                        int i = 1;
-                        ui.displayMsg(i + ") " + values[0] + " | Message: " + values[2] + "\nStatus: " + ui.promptTextColor(statusColor) + values[3] );
+                    }
+
+                    if(getTicketsByStatus("Open") == 0){
+                        ui.displayMsg(ui.promptTextColor("red") + "\nNo tickets open right now!" + ui.promptTextColor("reset"));
+                        startSession(username);
+                    }
+
+                    String supportInput = ui.promptTextLine("Choose a ticket by ID: ");
+
+                    if(supportInput.equals(values[0]) && values[3].equalsIgnoreCase("Open")){
+
+                        ui.displayMsg("\n_____________________________________\nReplying to ticket ID: " + values[0] + "\n\nMessage by user: \n" + values[2] + "\n");
+                        ui.displayMsg("Enter your message\n_________________________________");
+                        String supportMsg = ui.promptTextLine("Response: ");
+                        String status = ui.promptTextLine("Status on ticket: ");
+
+                        if(status.equalsIgnoreCase("Open")){
+                            status = "Open";
+                        } else if (status.equalsIgnoreCase("Solved")){
+                            status = "Solved";
+                        } else if (status.equalsIgnoreCase("Pending")){
+                            status = "Pending";
+                        } else {
+                            ui.displayMsg("Invalid status. Please only use (OPEN, SOLVED, PENDING)!");
+                            startSession(username);
+                        }
+
+                        Program.saveDataTicket(Integer.parseInt(values[0]), values[1], values[2], status, supportMsg, username);
+
+                        ui.displayMsg(ui.promptTextColor("green") + "\nMessage sent to " + values[0]
+                                + ui.promptTextColor("reset") + "\n____________________________");
+
+                        startSession(username);
 
                     }
 
+
                 }
+
                 break;
             case "2":
                 // Something
@@ -58,6 +98,33 @@ public class SupportMenu extends Menu{
                 ui.displayMsg("Closing program.. Thanks. Till next time!");
                 System.exit(0);
         }
+
+    }
+
+    // ________________________________________________________
+
+    public int getTicketsByStatus(String input){
+
+        ArrayList <String> data = io.readData("data/supportTickets.csv");
+
+        int statusByStatus = 0;
+
+        for (String s : data){
+
+            String[] values = s.split(", ");
+            String status = values[3].trim();
+
+            if(status.equalsIgnoreCase("Open") && input.equalsIgnoreCase("Open")){
+                statusByStatus++;
+            } else if (status.equalsIgnoreCase("Pending") && input.equalsIgnoreCase("Pending")){
+                statusByStatus++;
+            } else if (status.equalsIgnoreCase("Solved") && input.equalsIgnoreCase("Solved")) {
+                statusByStatus++;
+            }
+
+        } // For-loop end
+
+        return statusByStatus;
 
     }
 
